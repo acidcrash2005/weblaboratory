7725ea10f7801533f457d8d33e894ab7a9a25d71
unknown <acidcrash2005@gmail.com>
2017-06-23 10:36:53 +0300

    download 2

--- resources/views/user_page.blade.php
+++ resources/views/user_page.blade.php
@@ -6,49 +6,51 @@
 
         <div class="main-container">
 
-            <div class="new-mesages-block">
-                <h2 class="lesson-description__title">Ответы по Домашним Заданиям</h2>
-                <table class="table table-striped new-mesages__list">
-                    <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Курс</th>
-                        <th>Название урока</th>
-                    </tr>
-                    </thead>
-                    @foreach($HomeworkDialog as $dialog)
-                        @if($dialog->new_answer !=0)
+                <div class="new-mesages-block">
+                    <h2 class="lesson-description__title">Ответы по Домашним Заданиям</h2>
+                    <table class="table table-striped new-mesages__list">
+                        <thead>
                         <tr>
-                            <td>
-                                <a href="{{ url('/courses') }}/{{$dialog->lesson->cours->slug}}/{{$dialog->lesson->id}}#dz" class="msg-ico">
-
-                                   {{ Widget::run('NewMassage',['db' => 'HomeworkAnswer', 'dialog' => $dialog->id]) }} <i class="fa fa-envelope-o" aria-hidden="true"></i>
-
-                                </a>
-                            </td>
-                            <td>
-                                <a href="{{ url('/courses') }}/{{$dialog->lesson->cours->slug}}">
-                                    {{ $dialog->lesson->cours->title }}
-                                </a>
-                            </td>
-                            <td>
-                                <a href="{{ url('/courses') }}/{{$dialog->lesson->cours->slug}}/{{$dialog->lesson->id}}#dz">
-                                    {{ $dialog->lesson->title }}
-                                </a>
-                            </td>
-
+                            <th>#</th>
+                            <th>Курс</th>
+                            <th>Название урока</th>
                         </tr>
-                        @endif
-                    @endforeach
-                </table>
-            </div>
+                        </thead>
+                        @foreach($HomeworkDialog as $dialog)
+                            @if($dialog->new_answer !=0)
+                            <tr>
+                                <td>
+                                    <a href="{{ url('/courses') }}/{{$dialog->lesson->cours->slug}}/{{$dialog->lesson->id}}#dz" class="msg-ico">
 
-            <div class="cours-list-main">
-                <h2 class="lesson-description__title">Трансляции и вебинары</h2>
-                <div class="b-events">
-                    @include('layouts.courses.vebinars',$onAir)
+                                       {{ Widget::run('NewMassage',['db' => 'HomeworkAnswer', 'dialog' => $dialog->id]) }} <i class="fa fa-envelope-o" aria-hidden="true"></i>
+
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="{{ url('/courses') }}/{{$dialog->lesson->cours->slug}}">
+                                        {{ $dialog->lesson->cours->title }}
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="{{ url('/courses') }}/{{$dialog->lesson->cours->slug}}/{{$dialog->lesson->id}}#dz">
+                                        {{ $dialog->lesson->title }}
+                                    </a>
+                                </td>
+
+                            </tr>
+                            @endif
+                        @endforeach
+                    </table>
                 </div>
-            </div>
+
+            @if (!empty($onAir[0]))
+                <div class="cours-list-main">
+                    <h2 class="lesson-description__title">Трансляции и вебинары</h2>
+                    <div class="b-events">
+                        @include('layouts.courses.vebinars',$onAir)
+                    </div>
+                </div>
+            @endif
 
             <div class="cours-list-main">
                 <h2 class="lesson-description__title">Ваши курсы</h2>
